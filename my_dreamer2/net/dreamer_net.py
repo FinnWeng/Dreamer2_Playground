82eecfdac3099bc1b227096065b1c89ad171567b
finn.weng <finn.weng@coretronic.com>
2021-05-07 17:16:52 +0800

    replace onehot distribution in  get_dist with tools.oneot inorder to pass gradient

--- my_dreamer2/net/dreamer_net.py
+++ my_dreamer2/net/dreamer_net.py
@@ -320,10 +320,11 @@ class RSSM(tf.keras.Model):
         #     return tfd.MultivariateNormalDiag(state["mean"], state["std"])
         """
         if stop_grad:
-            return tfd.Independent(tfd.OneHotCategorical(tf.stop_gradient(state["logit_vector"])),1)
+            # return tfd.Independent(tfd.OneHotCategorical(tf.stop_gradient(state["logit_vector"])),1)
+            return tfd.Independent(tools.OneHotDist(tf.stop_gradient(state["logit_vector"])),1)
 
         else:
-            return tfd.Independent(tfd.OneHotCategorical(state["logit_vector"]),1)
+            return tfd.Independent((tools.OneHotDist(state["logit_vector"]),1))
 
     @tf.function
     def img_step(self, prev_state, prev_action):
